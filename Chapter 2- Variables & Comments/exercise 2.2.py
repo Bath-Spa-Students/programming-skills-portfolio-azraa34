c962e4422585c62571f2e3a5f4b6f13b5daafe97
azraa34 <144208795+azraa34@users.noreply.github.com>
2023-10-17 09:41:43 +0400

    exercise 2.2.py

---
+++ Chapter 2- Variables & Comments/exercise 2.2.py
@@ -0,0 +1,4 @@
+# Find a quote from a famous person you admire. Print the quote and the name of its author.
+
+print('Neil Gaiman once said, "Fairy tales are more than true: not because they tell us that')
+print('dragons exist, but because they tell us that dragons can be beaten."')
